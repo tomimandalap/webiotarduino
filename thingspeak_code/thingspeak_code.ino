72ded999539c75ecd3506336d5126e43b61c0b41
tomimandalap <tommymandalaputra@gmail.com>
2021-08-29 03:49:29 +0700

    feat: code thingspeak

---
+++ thingspeak_code/thingspeak_code.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include "DHT.h"
+
+// sensor DHT 11
+DHT dht(2, DHT11); //pin GPIO2
+
+// Initialize our values
+float temp, humd = 0;
+int soil = 0;
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+  dht.begin();
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  WiFi.mode(WIFI_STA); 
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+  // seensor dht  
+  humd = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  if (isnan(humd) || isnan(temp)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // sensor soil moisture (RANDOM VALUE)
+  soil = random(60,80);
+  
+  // set the fields with the values
+  ThingSpeak.setField(1, String(temp,2));
+  ThingSpeak.setField(2, String(humd,2));
+  ThingSpeak.setField(3, soil);
+  
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
